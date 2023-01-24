929cb7843bd910fbf6d89f92f86ebf977f02b546
Vignesh <vignesh@visionbox.ai>
2023-01-24 15:46:51 +0530

    disabled gitignore temporarliy

---
+++ powershell/profile.ps1
@@ -0,0 +1,5 @@
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "D:\ProgramData\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
---
+++ python/separate_files.py
@@ -0,0 +1,5 @@
+import os
+import glob
+p=r"E:\AI using python\appledai\[AppliedAI] Machine Learning Online Course\Module 8_ Neural Networks Computer Vision and Deep Learning"
+paths=glob.glob("E:\\AI using python\appledai\\[AppliedAI] Machine Learning Online Course\\Module 8_ Neural Networks Computer Vision and Deep Learning\\**\\*[!.mp4]")
+print(paths)
